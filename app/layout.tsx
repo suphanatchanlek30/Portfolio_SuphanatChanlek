f0b997a4f62a52e7e3ad738d8089eb0f12d81425
suphanatchanlek <suphanatchanlek@gmail.com>
2025-07-22 20:28:44 +0700

    add Components Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Manrope, Inter } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -9,6 +9,16 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const geistManrope = Manrope({
+  variable: "--font-manrope",
+  subsets: ["latin"],
+});
+
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -25,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${geistManrope.variable} ${inter.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import Navbar from '../components/Navbar';
+
 export default function Home() {
   return (
-    <div>test</div>
+    <div>
+      <Navbar />
+      <main className="container mx-auto p-8 bg-[#020202] text-white">
+        <h1>Welcome to Suphanat's Portfolio</h1>
+      </main>
+    </div>
   );
 }
